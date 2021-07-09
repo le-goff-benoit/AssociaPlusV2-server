9a6f7dc99cb53458633153a14dbcb369b6e27b68
Benoît Le Goff <benoit.le.goff@icloud.com>
2021-07-09 17:12:28 +0200

    Add User entity

---
+++ src/entities/User.ts
@@ -0,0 +1,32 @@
+import { Entity, PrimaryKey, Property } from "@mikro-orm/core";
+import { Field, Int, ObjectType } from "type-graphql";
+
+@Entity()
+@ObjectType()
+export class User {
+
+    @Field(() => Int)
+    @PrimaryKey()
+    id!: number;
+
+    @Field(() => String)
+    @Property({default: 'NOW()'})
+    createdAt: Date = new Date();
+
+    @Field(() => String)
+    @Property({ onUpdate: () => new Date() })
+    updatedAt: Date = new Date();
+
+    @Field(() => String)
+    @Property()
+    name!: string;
+
+    @Field(() => String)
+    @Property()
+    email!: string;
+
+    @Field(() => String)
+    @Property()
+    password!: string;
+
+}
